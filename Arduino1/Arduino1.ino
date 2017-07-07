0751dd900ef2c954ae78c5c9cfa110d1d7347aa2
Edik <Edik@EDIK-JABLKO>
2017-07-07 23:31:20 +0200

    Initial version

---
+++ Arduino1/Arduino1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Arduino1.ino
+ Created:	7/7/2017 9:38:07 PM
+ Author:	Edik
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
